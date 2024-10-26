b8e930e0b01b09bc8a383217332066ed715e7ac4
Jonas Dally <jonas.dally@hotmail.de>
2024-10-26 13:40:52 +0200

    create next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,27 @@
+import withPWAInit from 'next-pwa';
+import { InjectManifest } from 'workbox-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
+// Next.js configuration
 const nextConfig = {
-    images: {
-        domains: ['lh3.googleusercontent.com'],
-      },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new InjectManifest({
+          swSrc: './service-worker.js', 
+          swDest: 'service-worker.js',      
+        })
+      );
+    }
+    return config;
+  },
 };
 
-
-export default nextConfig;
+export default withPWA(nextConfig);
